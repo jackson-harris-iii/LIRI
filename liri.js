fa10f4abebda32f2d580e3f31ca6be2b83ac769b
Apollo <jackson.harris3@gmail.com>
2018-03-30 20:12:34 -0700

    compete omdb, update spotify

--- liri.js
+++ liri.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 
 var keys = require('./keys.js');
-var Twitter = require('twitter')
-var inquirer = require('inquirer')
-var Spotify = require('node-spotify-api')
+var Twitter = require('twitter');
+var inquirer = require('inquirer');
+var Spotify = require('node-spotify-api');
+var request = require('request');
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
@@ -11,7 +12,7 @@ var client = new Twitter(keys.twitter);
 var greetings = [
     {
         type: 'list',
-        choices: ['Twitter', 'Spotify', 'OMDb'],
+        choices: ['Twitter', 'Spotify', 'OMDb', 'Do what it says'],
         name: 'choice',
         message: "Hi I'm LIRI, What would you like to use?"
     }
@@ -62,6 +63,28 @@ var songTitle = [
 
 ]
 
+var omdbOptions = [
+    {
+        type:"list",
+        choices: [
+            {
+                name: 'by Title',
+                value: 'movieTitle'
+            }
+        ],
+        name: 'choice',
+        message: 'How would you like to look up a movie?'
+    }
+]
+
+var movieTitle = [
+    {
+        type: 'input',
+        message: "Enter the title of the movie you'd like some info on.",
+        name: 'input'
+    }
+]
+
 var prompt = inquirer.createPromptModule();
 
 prompt(greetings).then(answer => {
@@ -84,8 +107,36 @@ prompt(greetings).then(answer => {
                prompt(songTitle).then(response =>{
                    let song = response.input
                    let type = 'track'
-                   getSong(type, song)
+                   
+                   if (song === ''){
+                    console.log("You didn't enter a song so here's...")
+                    let noEntry = "The Sign Ace of Base"
+                    getSong(type, noEntry)
+                   }
+                   else{
+                    getSong(type, song)
+                   }
                })      
+            }
+        })
+    }
+
+    else if (options == 'omdbOptions') {
+        prompt(omdbOptions).then(response => {
+
+            if (response.choice == 'movieTitle') {
+                prompt(movieTitle).then(response => {
+                    let movie = response.input
+                    
+                    if (movie === ''){
+                        console.log("You didn't enter a movie so here's..." )
+                        let noEntry = "Mr. Nobody"
+                        getMovie(noEntry)
+                    }
+                    else{
+                        getMovie(movie)
+                    }
+                })
             }
         })
     }
@@ -123,4 +174,26 @@ function getTweets() {
           console.log('Preview URL' + data.tracks.items[0].preview_url)
           console.log('-------------') 
         })
+ }
+
+ function getMovie(movie) { 
+    let movieRequest = 'http://www.omdbapi.com/?apikey=trilogy&t=' + movie
+    request(movieRequest, function (error, response, body) {
+        if (error){
+            console.log('error:', error); // Print the error if one occurred
+        }
+        else {
+            let data = JSON.parse(body)
+            console.log('-------------')
+            console.log('Title: ' + data.Title)
+            console.log('Year: ' + data.Year)
+            console.log('IMDB Rating: ' + data.Ratings[0].Value)
+            console.log('Rotten Tomatoes Rating: ' + data.Ratings[1].Value)
+            console.log('Country: ' + data.Country)
+            console.log('Language: ' + data.Language)
+            console.log('Plot: ' + data.Plot)
+            console.log('Actors: ' + data.Actors)
+            console.log('-------------') 
+        }     
+    });        
  }
